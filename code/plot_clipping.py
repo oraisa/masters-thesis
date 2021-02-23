81ebba89381c8913d72a8dd61fda1398efc8a6c1
Ossi Räisä <ossi.raisa@gmail.com>
2021-02-23 12:29:16 +0200

    Clipping MMD baselines and y axis limits

--- code/plot_clipping.py
+++ code/plot_clipping.py
@@ -4,6 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt 
 import seaborn as sns 
 import banana_model
+import experiments
 import mmd
 import argparse
 
@@ -23,18 +24,23 @@ key = jax.random.PRNGKey(46237)
 keys = jax.random.split(key, 10)
 
 df2 = df[df["dim"] == 2]
-banana2 = banana_model.BananaModel(dim=2)
-data2 = banana2.generate_test_data()
-posterior2 = banana2.generate_posterior_samples(2000, data2, 1, keys[0])
-mmds2 = [mmd.mmd(banana2.generate_posterior_samples(1000, data2, 1, keys[i]), posterior2) for i in range(1, len(keys))]
+problem2 = experiments.experiments["easy-2d"].get_problem()
+mmds2 = [
+    mmd.mmd(
+        problem2.gen_true_posterior(1000, keys[i]),
+        problem2.true_posterior
+    ) for i in range(0, len(keys))
+]
 mmds2 = np.array(mmds2)
 
 df10 = df[df["dim"] == 10]
-# df10 = df10[df10["Algorithm"] == "HMC"]
-banana10 = banana_model.BananaModel(dim=10)
-data10 = banana10.generate_test_data()
-posterior10 = banana10.generate_posterior_samples(2000, data10, 1, keys[0])
-mmds10= [mmd.mmd(banana10.generate_posterior_samples(1000, data10, 1, keys[i]), posterior10) for i in range(1, len(keys))]
+problem10 = experiments.experiments["easy-10d"].get_problem()
+mmds10= [
+    mmd.mmd(
+        problem10.gen_true_posterior(1000, keys[i]),
+        problem10.true_posterior
+    ) for i in range(1, len(keys))
+]
 mmds10 = np.array(mmds10)
 
 fig, axes = plt.subplots(2, 2, figsize=(10, 5))
@@ -56,5 +62,10 @@ for mmd in mmds10:
 
 axes[0, 0].set_title("d = 2")
 axes[0, 1].set_title("d = 10")
+
+axes[0, 1].set_ylim(0, 0.6)
+axes[1, 0].set_ylim(0, 0.6)
+axes[1, 1].set_ylim(0, 0.6)
+axes[0, 0].set_ylim(0, 0.6)
 plt.tight_layout()
 plt.savefig("../Thesis/figures/clipping.pdf")
