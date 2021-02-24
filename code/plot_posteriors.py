Fix HMC, standardise start variance

code/plot_posteriors.py
@@ -11,7 +11,8 @@ plot_titles = [
 fig, axes = plt.subplots(3, 2, figsize=(8, 10))
 
 for i, exp in enumerate(exps_to_plot):
-    problem = experiments[exp].get_problem()
+    experiment = experiments[exp]
+    problem = experiment.get_problem()
     ind = np.unravel_index(i, axes.shape)
     problem.plot_density(axes[ind])
     axes[ind].set_title(plot_titles[i])

code/util.py
@@ -45,13 +45,15 @@ class Problem:
             # inters = np.quantile(self.true_posterior, (0.25, 0.75), axis=0)
             # mul = (inters[1,:] - inters[0,:]).mean() * 0.5
             # print(mul)
-            # mul = self.true_posterior.std(axis=0).mean() * 0.5
-            ind = jax.random.choice(key, self.true_posterior.shape[0])
-            return self.true_posterior[ind, :]
+            mul = self.true_posterior.std(axis=0).mean() * 1
+            # ind = jax.random.choice(key, self.true_posterior.shape[0])
+            # return self.true_posterior[ind, :]
             # print(mul)
         else:
-            std_noise = jax.random.normal(key, shape=(self.dim,))
-            return self.theta0 + std_noise * 0.3
+            mul = 1
+            # std_noise = jax.random.normal(key, shape=(self.dim,))
+            # return self.theta0 + std_noise * 0.3
+        return self.theta0 + jax.random.normal(key, shape=(self.dim,)) * mul
 
 def clip_grad_fun(grad_fun):
     def return_fun(clip, *args):